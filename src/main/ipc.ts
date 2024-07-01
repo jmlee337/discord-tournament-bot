52615e8ffa82cab8614ec93e3ec2e153d4ee954e
Nicolet <jmlee337@gmail.com>
2024-07-01 22:13:44 +0900

    support multiple sets per entrant (RR)
    fixes #8

--- src/main/ipc.ts
+++ src/main/ipc.ts
@@ -2,13 +2,19 @@ import {
   ActionRowBuilder,
   ButtonBuilder,
   ButtonStyle,
+  CacheType,
+  ChatInputCommandInteraction,
   Client,
   EmbedBuilder,
   Events,
   GatewayIntentBits,
+  InteractionResponse,
   REST,
   Routes,
   SlashCommandBuilder,
+  StringSelectMenuBuilder,
+  StringSelectMenuInteraction,
+  StringSelectMenuOptionBuilder,
 } from 'discord.js';
 import {
   BrowserWindow,
@@ -35,6 +41,44 @@ import {
   swapWinner,
 } from './startgg';
 
+const CONFIRMATION_TIMEOUT_MS = 30000;
+function setInitialEmbed(embed: EmbedBuilder, set: StartggSet) {
+  return embed
+    .setColor('#3870e0')
+    .setTitle(`${set.entrant1Name} vs ${set.entrant2Name}`)
+    .setDescription(set.fullRoundText)
+    .setFooter({ text: 'Please click the set winner' });
+}
+
+function getButtonRow(set: StartggSet) {
+  return new ActionRowBuilder<ButtonBuilder>().addComponents(
+    new ButtonBuilder()
+      .setCustomId(set.entrant1Id.toString(10))
+      .setLabel(set.entrant1Name)
+      .setStyle(ButtonStyle.Success),
+    new ButtonBuilder()
+      .setCustomId(set.entrant2Id.toString(10))
+      .setLabel(set.entrant2Name)
+      .setStyle(ButtonStyle.Success),
+  );
+}
+
+function timeOutInteraction(
+  embed: EmbedBuilder,
+  interaction:
+    | ChatInputCommandInteraction<CacheType>
+    | StringSelectMenuInteraction<CacheType>,
+) {
+  interaction.editReply({
+    components: [],
+    embeds: [
+      embed
+        .setColor('#031221')
+        .setFooter({ text: 'Timed out. You may try again' }),
+    ],
+  });
+}
+
 export default function setupIPCs(mainWindow: BrowserWindow) {
   const store = new Store();
   let discordConfig = store.has('discordConfig')
@@ -52,7 +96,7 @@ export default function setupIPCs(mainWindow: BrowserWindow) {
    */
   const discordIdToEntrantId = new Map<string, number>();
   const entrantIdToDiscordIds = new Map<number, string[]>();
-  const entrantIdToSet = new Map<number, StartggSet>();
+  const entrantIdToSets = new Map<number, StartggSet[]>();
   let eventId = 0;
   let eventName = '';
   let sets: Sets = {
@@ -65,12 +109,16 @@ export default function setupIPCs(mainWindow: BrowserWindow) {
     events: [],
   };
   const updateEntrantIdToSet = (newSets: Sets) => {
-    entrantIdToSet.clear();
+    entrantIdToSets.clear();
     newSets.pending.forEach((pendingPhase) => {
       pendingPhase.phaseGroups.forEach((pendingPhaseGroup) => {
         pendingPhaseGroup.sets.forEach((pendingSet) => {
-          entrantIdToSet.set(pendingSet.entrant1Id, pendingSet);
-          entrantIdToSet.set(pendingSet.entrant2Id, pendingSet);
+          const entrant1Sets = entrantIdToSets.get(pendingSet.entrant1Id) || [];
+          entrant1Sets.push(pendingSet);
+          entrantIdToSets.set(pendingSet.entrant1Id, entrant1Sets);
+          const entrant2Sets = entrantIdToSets.get(pendingSet.entrant2Id) || [];
+          entrant2Sets.push(pendingSet);
+          entrantIdToSets.set(pendingSet.entrant2Id, entrant2Sets);
         });
       });
     });
@@ -115,6 +163,67 @@ export default function setupIPCs(mainWindow: BrowserWindow) {
       updateDiscordStatus(DiscordStatus.BAD_APPLICATION_ID);
     }
   };
+  const awaitReportResponse = async (
+    embed: EmbedBuilder,
+    interaction:
+      | ChatInputCommandInteraction<CacheType>
+      | StringSelectMenuInteraction<CacheType>,
+    response: InteractionResponse<boolean>,
+    set: StartggSet,
+  ) => {
+    const validDiscordIds = new Set<string>();
+    const forEachPredicate = (discordId: string) => {
+      validDiscordIds.add(discordId);
+    };
+    entrantIdToDiscordIds.get(set.entrant1Id)?.forEach(forEachPredicate);
+    entrantIdToDiscordIds.get(set.entrant2Id)?.forEach(forEachPredicate);
+    try {
+      const confirmation = await response.awaitMessageComponent({
+        time: CONFIRMATION_TIMEOUT_MS,
+        filter: (confI) => validDiscordIds.has(confI.user.id),
+      });
+      confirmation.update({
+        components: [],
+        embeds: [
+          embed.setColor('#68717a').setFooter({
+            text: 'Reporting to start.gg...',
+          }),
+        ],
+      });
+      const winnerId = parseInt(confirmation.customId, 10);
+      try {
+        await reportSet(
+          { setId: set.id, winnerId, isDQ: false },
+          startggApiKey,
+        );
+        const inner = winnerId === set.entrant1Id ? '[W - L]' : '[L - W]';
+        const reporterName =
+          discordIdToEntrantId.get(confirmation.user.id)! === set.entrant1Id
+            ? set.entrant1Name
+            : set.entrant2Name;
+        confirmation.editReply({
+          components: [],
+          embeds: [
+            embed
+              .setColor('#22b24c')
+              .setTitle(`${set.entrant1Name}  ${inner}  ${set.entrant2Name}`)
+              .setFooter({ text: `Reported by ${reporterName}` }),
+          ],
+        });
+      } catch {
+        confirmation.editReply({
+          components: [],
+          embeds: [
+            embed.setColor('#e0225b').setFooter({
+              text: 'Failed to report to start.gg. Please try again',
+            }),
+          ],
+        });
+      }
+    } catch {
+      timeOutInteraction(embed, interaction);
+    }
+  };
   const maybeStartDiscordClient = async () => {
     if (
       discordConfig.applicationId &&
@@ -150,93 +259,73 @@ export default function setupIPCs(mainWindow: BrowserWindow) {
             });
             return;
           }
-          const set = entrantIdToSet.get(entrantId);
-          if (!set) {
+          const entrantSets = entrantIdToSets.get(entrantId);
+          if (!entrantSets) {
             interaction.reply({ content: 'No set to report', ephemeral: true });
             return;
           }
-          const embed = new EmbedBuilder()
-            .setColor('#3870e0')
-            .setTitle(`${set.entrant1Name} vs ${set.entrant2Name}`)
-            .setDescription(set.fullRoundText)
-            .setFooter({ text: 'Please click the set winner' });
-          const response = await interaction.reply({
-            components: [
-              new ActionRowBuilder<ButtonBuilder>().addComponents(
-                new ButtonBuilder()
-                  .setCustomId(set.entrant1Id.toString(10))
-                  .setLabel(set.entrant1Name)
-                  .setStyle(ButtonStyle.Success),
-                new ButtonBuilder()
-                  .setCustomId(set.entrant2Id.toString(10))
-                  .setLabel(set.entrant2Name)
-                  .setStyle(ButtonStyle.Success),
-              ),
-            ],
-            embeds: [embed],
-          });
-
-          const validDiscordIds = new Set<string>();
-          const forEachPredicate = (discordId: string) => {
-            validDiscordIds.add(discordId);
-          };
-          entrantIdToDiscordIds.get(set.entrant1Id)?.forEach(forEachPredicate);
-          entrantIdToDiscordIds.get(set.entrant2Id)?.forEach(forEachPredicate);
-          try {
-            const confirmation = await response.awaitMessageComponent({
-              time: 15000,
-              filter: (confI) => validDiscordIds.has(confI.user.id),
+          if (entrantSets.length === 1) {
+            const [set] = entrantSets;
+            const embed = setInitialEmbed(new EmbedBuilder(), set);
+            const response = await interaction.reply({
+              components: [getButtonRow(set)],
+              embeds: [embed],
             });
-            confirmation.update({
-              components: [],
-              embeds: [
-                embed.setColor('#68717a').setFooter({
-                  text: 'Reporting to start.gg...',
-                }),
-              ],
-            });
-            const winnerId = parseInt(confirmation.customId, 10);
-            try {
-              await reportSet(
-                { setId: set.id, winnerId, isDQ: false },
-                startggApiKey,
+            await awaitReportResponse(embed, interaction, response, set);
+          } else {
+            const embed = new EmbedBuilder()
+              .setColor('#3870e0')
+              .setTitle(
+                `${interaction.user.displayName} selecting set to report...`,
               );
-              const inner = winnerId === set.entrant1Id ? '[W - L]' : '[L - W]';
-              const reporterName =
-                discordIdToEntrantId.get(confirmation.user.id)! ===
-                set.entrant1Id
-                  ? set.entrant1Name
-                  : set.entrant2Name;
-              confirmation.editReply({
-                components: [],
-                embeds: [
-                  embed
-                    .setColor('#22b24c')
-                    .setTitle(
-                      `${set.entrant1Name}  ${inner}  ${set.entrant2Name}`,
-                    )
-                    .setFooter({ text: `Reported by ${reporterName}` }),
-                ],
-              });
-            } catch {
-              confirmation.editReply({
-                components: [],
-                embeds: [
-                  embed.setColor('#e0225b').setFooter({
-                    text: 'Failed to report to start.gg. Please try again',
-                  }),
-                ],
-              });
-            }
-          } catch {
-            interaction.editReply({
-              components: [],
-              embeds: [
-                embed
-                  .setColor('#031221')
-                  .setFooter({ text: 'Timed out. You may try again' }),
+            const response = await interaction.reply({
+              components: [
+                new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
+                  new StringSelectMenuBuilder()
+                    .setCustomId('asdf')
+                    .setPlaceholder('Which set would you like to report?')
+                    .addOptions(
+                      entrantSets.map((entrantSet) =>
+                        new StringSelectMenuOptionBuilder()
+                          .setLabel(
+                            `vs ${
+                              entrantId === entrantSet.entrant1Id
+                                ? entrantSet.entrant2Name
+                                : entrantSet.entrant1Name
+                            }`,
+                          )
+                          .setValue(entrantSet.id.toString(10)),
+                      ),
+                    ),
+                ),
               ],
+              embeds: [embed],
             });
+            try {
+              const selectSetConfirmation =
+                await response.awaitMessageComponent({
+                  time: CONFIRMATION_TIMEOUT_MS,
+                  filter: (confI) => confI.user.id === interaction.user.id,
+                });
+              if (selectSetConfirmation.isStringSelectMenu()) {
+                const setId = parseInt(selectSetConfirmation.values[0], 10);
+                const entrantSet = entrantSets.find((set) => set.id === setId);
+                if (entrantSet) {
+                  const reportSetResponse = await selectSetConfirmation.update({
+                    components: [getButtonRow(entrantSet)],
+                    embeds: [setInitialEmbed(embed, entrantSet)],
+                  });
+                  await awaitReportResponse(
+                    embed,
+                    selectSetConfirmation,
+                    reportSetResponse,
+                    entrantSet,
+                  );
+                }
+              }
+            } catch {
+              timeOutInteraction(embed, interaction);
+            }
           }
         } else {
           interaction.reply(interaction.commandName);
